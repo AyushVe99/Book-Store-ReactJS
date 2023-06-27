6207a79fba9e3de6ed8febe9795a08230cbf3626
AyushVe99 <Ayushnishu407@gmail.com>
2023-06-28 01:59:56 +0530

    Updates

---
+++ precache-manifest.895f78a16e9449190db9c49533807a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b706d2f8eb39c884c9593c4283e107c0",
+    "url": "/Book-Store-ReactJS/index.html"
+  },
+  {
+    "revision": "4b50e57bd6957337334a",
+    "url": "/Book-Store-ReactJS/static/css/main.3daecb8f.chunk.css"
+  },
+  {
+    "revision": "6137ace1d728a8b66f80",
+    "url": "/Book-Store-ReactJS/static/js/2.3e2f4ed5.chunk.js"
+  },
+  {
+    "revision": "7c8b7808c34cb59049374f5d75e06b5f",
+    "url": "/Book-Store-ReactJS/static/js/2.3e2f4ed5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b50e57bd6957337334a",
+    "url": "/Book-Store-ReactJS/static/js/main.7c0265e2.chunk.js"
+  },
+  {
+    "revision": "59873f11756b2d80349e",
+    "url": "/Book-Store-ReactJS/static/js/runtime-main.1ac32330.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/myapp/precache-manifest.57d3835e24a614b2e6e69fa6d1285958.js"
+  "/Book-Store-ReactJS/precache-manifest.895f78a16e9449190db9c49533807a01.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Book-Store-ReactJS/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
